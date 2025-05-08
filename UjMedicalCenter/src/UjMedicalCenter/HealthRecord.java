134c0171c37cdde3a813b7de46ba93f3cba147f3
Sadeem Aljahdali <121310002+SadeemJah@users.noreply.github.com>
2025-05-08 15:34:53 +0300

    Update HealthRecord.java

--- UjMedicalCenter/src/UjMedicalCenter/HealthRecord.java
+++ UjMedicalCenter/src/UjMedicalCenter/HealthRecord.java
@@ -1,19 +1,20 @@
 package UjMedicalCenter;
 
-
 import java.util.ArrayList;
 
-// Applying Composite Pattern HealthRecord implements RecordComponent interface
-public class HealthRecord {
+public class HealthRecord implements Subject {
     private Student student;
     private ArrayList<RecordComponent> notes = new ArrayList<>();
-    
+    private ArrayList<Observer> observers = new ArrayList<>();
+
     public HealthRecord(Student student) {
         this.student = student;
+        registerObserver(new StudentSMSObserver(student.getPhoneNumber()));
     }
 
     public void addNote(RecordComponent note) {
         notes.add(note);
+        notifyObservers("A new note has been added to your health record: " + note.getContent());
     }
 
     public ArrayList<RecordComponent> getNotes() {
@@ -22,8 +23,26 @@ public class HealthRecord {
 
     public void showRecord() {
         System.out.println("\uD83E\uDE7A Health Record for " + student.getName());
-         for (RecordComponent note : notes) {
+        for (RecordComponent note : notes) {
             note.showDetails();
+        }
+    }
+
+    // Observer pattern methods
+    @Override
+    public void registerObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    @Override
+    public void removeObserver(Observer observer) {
+        observers.remove(observer);
+    }
+
+    @Override
+    public void notifyObservers(String message) {
+        for (Observer observer : observers) {
+            observer.update(message);
         }
     }
 }
