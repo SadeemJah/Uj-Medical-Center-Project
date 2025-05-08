14af401ecff3109868b71d179bf638da65e2e157
Sadeem Aljahdali <121310002+SadeemJah@users.noreply.github.com>
2025-05-08 15:37:49 +0300

    Update RecordManager.java

--- UjMedicalCenter/src/UjMedicalCenter/RecordManager.java
+++ UjMedicalCenter/src/UjMedicalCenter/RecordManager.java
@@ -1,33 +1,30 @@
 package UjMedicalCenter;
 
-
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
-    //Singleton pattern
-    public class RecordManager {
-        private static RecordManager instance;
+        //Singleton pattern
+        public class RecordManager {
+            private static RecordManager instance;
 
-        //private constructor to prevent direct instantiation
-        private RecordManager() {}
+            // private constructor to prevent direct instantiation
+            private RecordManager() {}
 
-        //public method to get the one and only instance
-        public static RecordManager getInstance() {
-            if (instance == null) {
-                instance = new RecordManager();
+            // public method to get the one and only instance
+            public static RecordManager getInstance() {
+                if (instance == null) {
+                    instance = new RecordManager();
+                }
+                return instance;
             }
-            return instance;
-        }
-
-        
-        public static void saveStudentRecords(ArrayList<Student> students, String filename) {
+            
+            public static void saveStudentRecords(ArrayList<Student> students, String filename) {
             try (FileWriter writer = new FileWriter(filename)) {
                 for (Student student : students) {
                     writer.write("Student ID: " + student.getID() + "\n");
                     writer.write("Name: " + student.getName() + "\n");
                     writer.write("Phone: " + student.getPhoneNumber() + "\n");
-                    writer.write("Branch: " + student.getBranch() + "\n");
 
                     writer.write("Appointments:\n");
                     for (Appointment appointment : student.getAppointments()) {
@@ -35,9 +32,10 @@ import java.util.ArrayList;
                     }
 
                     writer.write("Health Record:\n");
-                    for (String note : student.getHealthRecord().getNotes()) {
-                        writer.write("- " + note + "\n");
+                    for (RecordComponent note : student.getHealthRecord().getNotes()) {
+                        writer.write("- " + note.getContent() + "\n");
                     }
+
 
                     writer.write("--------------------------------------------------\n");
                 }
